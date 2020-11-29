bdc58d16e24541b82f55b241a471c6209abe19b6
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-11-29 17:30:21 -0600

    user signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 
 // importes routes
@@ -19,9 +20,10 @@ mongoose.connect(process.env.DATABASE, {
 .then(() => console.log("DB is Connected ..."));
 
 //middlewares
-app.use(morgan("dev"))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
 //routes middleware
 app.use("/api",userRoutes);
