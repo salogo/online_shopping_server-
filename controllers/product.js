2d3736118de43c43f1dd8bbb2967baeb6e1b1060
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-06 23:37:15 -0600

    finding a single product by its id

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,23 @@ const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 const { result } = require("lodash");
 
+exports.productById = (req, res, next,id) => {
+   Product.findById(id).exec((err, product) => {
+       if(err || !product) {
+           return res.status(400).json({
+               error: "Product not found"
+           });
+       }
+       req.product = product;
+       next();
+   });
+}
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
 
 exports.create = (req, res) => {
    let form = new formidable.IncomingForm()//to allow any data type
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+
+router.get("/product/:productId", read)
 router.post("/product/create/:userId",requireSignin, isAuth,isAdmin , create );
 
 
 //anytime there parameter called userId in the route,
 // the userById mothode will execute and provide the user profile info
 router.param("userId", userById);
+// same exampl for parameter  
+router.param("productId", productById)
 
 module.exports = router;
