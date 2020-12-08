3a2a139f43af714f1c57ee331959883632775f94
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-08 17:57:08 -0600

    update a product

--- controllers/product.js
+++ controllers/product.js
@@ -74,3 +74,42 @@ exports.remove = (req, res) => {
         });
     });
 };
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()//to allow any data type
+    form.keepExtensions = true // any data extension 
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+            // check for all fields
+            const {name, description,price, category, quantity, shipping} = fields
+            if(! name || !description || !price || !category || !quantity || !shipping) {
+                return res.status(400).json({
+                    error: "All field are required"
+                })
+            }
+        let product = req.product;
+        product = _.extend(product, fields);
+        if(files.photo) {  
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image size must be less than 1mb"
+                });
+            } 
+        
+              product.photo.data = fs.readFileSync(files.photo.path)
+              product.photo.contenType = files.photo.type        
+        }
+        product.save(() => {
+            if(err) {
+               return res.status(400).json({
+                   error: errorHandler(err)
+               });
+            }
+            res.json(result);
+        });
+    });
+ };
