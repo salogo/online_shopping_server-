a905cdad983b5bf19e691f3e3d8a4c3cf8f63f1c
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-10 14:41:59 -0600

    update ,remove, list categories

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, categoryById, read } = require("../controllers/category");
+const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+//const { update } = require("../models/user");
 
 router.get("/category/:categoryId", read);
 router.post("/category/create/:userId",requireSignin, isAuth,isAdmin , create );
+router.put("/category/:categoryId/:userId",requireSignin, isAuth,isAdmin , update );
+router.delete("/category/:categoryId/:userId",requireSignin, isAuth,isAdmin , remove );
+router.get("/categories", list);
 
 //category middleware
 router.param("categoryId", categoryById);
