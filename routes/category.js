55a3a7ec29e1635a7eb1e770ab58ebaf6ef602bf
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-11 16:56:02 -0600

    fetch product based on queries selected

--- routes/category.js
+++ routes/category.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
-//const { update } = require("../models/user");
+
 
 router.get("/category/:categoryId", read);
 router.post("/category/create/:userId",requireSignin, isAuth,isAdmin , create );
