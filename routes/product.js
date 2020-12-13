0c6a15e1d0248c081d7135302c81970decc08097
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-13 17:16:37 -0600

    list product by search and display product photo

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated, listCategories } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -12,7 +12,10 @@ router.delete("/product/:productId/:userId", requireSignin,isAuth,isAdmin,remove
 router.put("/product/:productId/:userId", requireSignin,isAuth,isAdmin,update);
 router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
-router.get("/products/categories", listCategories)
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
+
 
 //anytime there parameter called userId in the route,
 // the userById mothode will execute and provide the user profile info
