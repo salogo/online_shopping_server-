dd3d3560be141e44c193e902f71a69e792f7339f
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-15 18:24:03 -0600

    display user profile and update

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,31 @@ exports.userById = (req, res, next, id) => {
                 error: "User not found" 
             });
         }
-        req.profile = user;
+        req.profile = user;     
         next();
     });
 };
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        {$set: req.body},
+        {new: true},
+        (err, user) => {
+            if(err) {
+                return res.status(400).json({
+                    error: "You are not authorized"
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        }
+    );
+}
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,19 @@
 const express = require("express");
+//const { update } = require("lodash");
 const router = express.Router();
 
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { userById} = require("../controllers/user");
+const { userById, read, update} = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin,isAuth, isAdmin, (req,res) => {
     res.json({
         user: req.profile
     });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
 
 //anytime there parameter called userId in the route,
 // the userById mothode will execute and provide the user profile info
