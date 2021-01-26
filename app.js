3a384ee3b7960c47a46dd650ceb4d75d07a18397
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-01-26 16:00:51 -0600

    fixing routes bugs

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product")
+const productRoutes = require("./routes/product");
 const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require("./routes/order");
 
 //app
 const app = express();
@@ -37,6 +38,7 @@ app.use("/api",userRoutes);
 app.use("/api",categoryRoutes);
 app.use("/api",productRoutes);
 app.use('/api', braintreeRoutes);
+app.use("/api", orderRoutes);
 
 
 const port = process.env.PORT || 8000
