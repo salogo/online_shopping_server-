9e238934577462d020b94a1e5e0cb306e36a63e5
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-02-02 04:16:23 -0600

    user purcahse history

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
+const { Order } = require("../models/order");
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -65,3 +68,17 @@ exports.addOrderToUserHistory = (req, res, next) => {
         }
     );
 };
+
+exports.purchaseHistory = (req, res) => {
+    Order.find({user: req.profile._id})
+    .populate("user", "_id name")
+    .sort("-create")
+    .exec((err, orders) => {
+       if (err) {
+           return res.status(400).json({
+               error: errorHandler(err)
+           })
+       }
+       res.json(orders)
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { userById, read, update} = require("../controllers/user");
+const { userById, read, update, purchaseHistory} = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin,isAuth, isAdmin, (req,res) => {
     res.json({
@@ -14,6 +14,7 @@ router.get("/secret/:userId", requireSignin,isAuth, isAdmin, (req,res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
 
 //anytime there parameter called userId in the route,
 // the userById mothode will execute and provide the user profile info
